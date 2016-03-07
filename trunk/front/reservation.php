c79065bf0620cf7b8e3821ce1d2cc0defedb44d2
salleman <salleman@a121aae4-8c90-420c-a72f-19c38f356383>
2016-03-07 14:34:31 +0000

    correction d'un pb sur IIS qui accepte pas la methode getAbsolutePath...
    git-svn-id: https://forge.glpi-project.org/svn/reservation@29 a121aae4-8c90-420c-a72f-19c38f356383

--- trunk/front/reservation.php
+++ trunk/front/reservation.php
@@ -2,9 +2,9 @@
 
 
 // Définition de la variable GLPI_ROOT obligatoire pour l'instanciation des class
-define('GLPI_ROOT', getAbsolutePath());
+//define('GLPI_ROOT', getAbsolutePath());
 // Récupération du fichier includes de GLPI, permet l'accès au cœur
-include (GLPI_ROOT."inc/includes.php");
+include ("../../../inc/includes.php");
 
 
 
