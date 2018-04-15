c9c82877783d427a21d97f73256a8d49244aed64
Sylvain Allemand <sylvain.allemand@math.u-bordeaux.fr>
2018-04-15 17:24:01 +0200

    changement config wip

--- hook.php
+++ hook.php
@@ -38,10 +38,9 @@ function plugin_reservation_install() {
    if (!TableExists("glpi_plugin_reservation_configs")) { //INSTALL >= 1.5.0
       $query = "CREATE TABLE `glpi_plugin_reservation_configs` (
                 `id` int(11) NOT NULL AUTO_INCREMENT,
-                `name` VARCHAR(10) NOT NULL,
+                `name` VARCHAR(10) NOT NULL UNIQUE,
                 `value` VARCHAR(10) NOT NULL,
                 PRIMARY KEY (`id`),
-                UNIQUE (`name`),
                 )ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci";
       $DB->queryOrDie($query, $DB->error());
 
