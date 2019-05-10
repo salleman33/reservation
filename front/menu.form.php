18f8a1da70f9c06bca67cadfc5a0510ea1eba137
Sylvain Allemand <sylvain.allemand@math.u-bordeaux.fr>
2019-05-11 00:05:33 +0200

    test define GLPI_ROOT

--- front/menu.form.php
+++ front/menu.form.php
@@ -1,4 +1,11 @@
 <?php
 
-include '../../../inc/includes.php';
+if (!defined('GLPI_ROOT')) {
+    die("Sorry. You can't access directly to this file");
+ }
+// // Définition de la variable GLPI_ROOT obligatoire pour l'instanciation des class
+// define('GLPI_ROOT', getAbsolutePath());
+// // Récupération du fichier includes de GLPI, permet l'accès au cœur
+// include GLPI_ROOT . "inc/includes.php";
+// // include '../../../inc/includes.php';
 Html::back();
