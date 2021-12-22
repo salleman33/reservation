a7e17c327d1c145998f3ded02526602f3dbb602d
Sylvain Allemand <sylvain.allemand@math.u-bordeaux.fr>
2021-12-22 10:16:04 +0100

    on ignore les reservations deja terminées - un bug peut se produire si une personne faire une reservation dans le passé

--- inc/task.class.php
+++ inc/task.class.php
@@ -129,6 +129,7 @@ class PluginReservationTask extends CommonDBTM
 
          $conflict_reservations = PluginReservationReservation::getAllReservations(["`begin` >= '".$reservation->fields['end']."'",
                                                                            "`begin` <= '".$end."'",
+                                                                           "`effectivedate` != NULL",
                                                                            "reservationitems_id = ".$res['reservationitems_id']]);
 
          $query = "UPDATE `glpi_reservations` 
