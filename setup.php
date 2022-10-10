5659ed8e4e1b19d9e5667cfeec8eb646aacc180d
Sylvain Allemand <sylvain.allemand@math.u-bordeaux.fr>
2022-10-10 14:43:57 +0200

    v2.4.1

--- setup.php
+++ setup.php
@@ -1,7 +1,7 @@
 <?php
 
 define('PLUGIN_RESERVATION_GLPI_MIN_VERSION', '10');
-define('PLUGIN_VERSION', '2.4.0');
+define('PLUGIN_VERSION', '2.4.1');
 
 /**
  * Init the hooks of the plugins - Needed
